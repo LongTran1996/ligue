1fc86aa536c4a15dc9f24a138abe88f25fd98397
Samuel Mercier-Poirier <mercier_poirier_samuel@msn.com>
2017-04-21 11:07:54 -0400

    role et commence wep.php

--- ligue/routes/web.php
+++ ligue/routes/web.php
@@ -11,6 +11,33 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('home');
+
+Route::get('/matchs/create', 'MatchController@create')->middleware('admin');;
+
+// Présentation d'un match
+Route::get('/matchs/{match}', 'MatchController@show');
+
+// Sauvegarde d'un match lors de la création
+// Restreint aux rôles : admin
+Route::post('/matchs', 'MatchController@store');
+
+// Page de gestion des matchs pour les administrateurs
+// Restreint aux rôles : admin 
+Route::get('/matchs/edit', 'MatchController@Edit')->middleware('admin');
+
+// Page de gestion des matchs pour les administrateurs pour une équipe
+// Restreint aux rôles : admin 
+Route::get('/matchs/edit/{team}', 'MatchController@teamEdit')->middleware('admin');
+
+// Modifier un match
+// Restreint aux rôles : admin
+Route::get('/matchs/edit/{match}', 'MatchController@update')->middleware('admin');
+
+// Sauvegarde des modifications d'un match
+// Restreint aux rôles : admin
+Route::post('/matchs/edit/{match}', 'MatchController@store')->middleware('admin');
+
+// Efface un match
+// Restreint aux rôles : admin 
+Route::get('/matchs/delete/{match}', 'MatchController@delete')->middleware('admin');
